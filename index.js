1ebe0bde3a60452f965e8fd15e89e05493a61bbf
Riku <rikunummi9@gmail.com>
2023-09-08 14:06:42 +0300

    3.1-3.6 tehtävät done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
 let persons = [
   {
@@ -15,13 +16,13 @@ let persons = [
   },
   {
     id: 3,
-    name: "Dan Abramov",
+    name: "Dani Abrahamovitsi",
     number: "04053745369"
   },
   {
     id: 4,
     name: "Mary Poppins",
-    number: "040048329589"
+    number: "13453295843958943"
   }
 ]
 app.get('/', (req, res) => {
@@ -30,6 +31,70 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get('/info', (req, res) => {
+  res.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+const generateId = () => {
+  const min = 4
+  const max = 1000000
+  const randomId = Math.floor(Math.random() * (max - min + 1)) + min
+  
+  return randomId
+  }
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  const uniqueCheck = persons.some((person) => person.name === body.name)
+
+  if (uniqueCheck) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
 })
 
 const PORT = 3001
