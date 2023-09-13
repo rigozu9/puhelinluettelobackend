6d594a83ba458ef82dbe90a48e53dc39e3cc4d6f
Riku <rikunummi9@gmail.com>
2023-09-13 12:31:53 +0300

    dist lisätty for frontend

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
-
 const morgan = require('morgan')
-
 const cors = require('cors')
 app.use(cors())
-
 app.use(express.json())
+app.use(express.static('dist'))
 
 let persons = [
   {
